f242c1cb9092de354edb858f2dd4a09698a51b2f
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-07-06 13:03:10 -0700

    First commit

---
+++ SolarSystem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SolarSystem
+//
+//  Created by Yuliya  on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SolarSystem/SolarSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemApp.swift
+//  SolarSystem
+//
+//  Created by Yuliya  on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
